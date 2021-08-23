62437fc5e992b3465eb7b07e1902946d5fc2b2e8
icowen <icowen@luc.edu>
2021-08-23 17:02:29 -0500

    Basic env setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,23 @@
-import logo from '../logo.svg';
-import '../styling/App.css';
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Stars } from "@react-three/drei";
+import { Physics } from "@react-three/cannon";
+import "../styling/App.css";
+import Pig from "./Pig";
+import Ground from "./Ground";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Canvas>
+      <OrbitControls />
+      <Stars />
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 15, 10]} angle={0.3} />
+      <Physics>
+        <Pig />
+        <Ground />
+      </Physics>
+    </Canvas>
   );
 }
 
