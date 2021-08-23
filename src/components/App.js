fcdcc2235a3496cb6deaf334d1de1064e910e576
icowen <icowen@luc.edu>
2021-08-23 18:01:08 -0500

    beginning to roll

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Stars } from "@react-three/drei";
 import { Physics } from "@react-three/cannon";
@@ -7,17 +7,24 @@ import Pig from "./Pig";
 import Ground from "./Ground";
 
 function App() {
+  const [roll, setRoll] = useState(true);
+
   return (
-    <Canvas>
-      <OrbitControls />
-      <Stars />
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 15, 10]} angle={0.3} />
-      <Physics>
-        <Pig />
-        <Ground />
-      </Physics>
-    </Canvas>
+    <Fragment>
+      <div className={"roll-pigs button"} onClick={() => setRoll(!roll)}>
+        {"Roll"}
+      </div>
+      <Canvas>
+        <OrbitControls />
+        <Stars />
+        <ambientLight intensity={0.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <Physics>
+          <Pig roll={roll} setRoll={setRoll}/>
+          <Ground />
+        </Physics>
+      </Canvas>
+    </Fragment>
   );
 }
 
--- src/components/Pig.jsx
+++ src/components/Pig.jsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useBox } from "@react-three/cannon";
 
-export default function Pig() {
+export default function Pig({ roll, setRoll }) {
   const [ref, api] = useBox(() => ({ mass: 1, position: [0, 2, 0] }));
+
+  useEffect(() => {
+    console.log("Called with roll: ", roll);
+    api.velocity.set(0, 5, 0);
+    setRoll(false);
+  }, [roll, setRoll]);
+
   return (
     <mesh
       onClick={() => {
